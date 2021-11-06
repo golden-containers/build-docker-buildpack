007fe6595784bd77fc74a201c709ebeba19ea312
Jeremiah Gowdy <jgowdy@godaddy.com>
2021-11-05 19:32:14 -0700

    Update checkout-buildpack.sh

--- checkout-buildpack.sh
+++ checkout-buildpack.sh
@@ -2,8 +2,8 @@ rm -rf buildpack-deps
 git clone --depth 1 --branch master https://github.com/docker-library/buildpack-deps.git
 cd buildpack-deps
 
-sed -i -e "1 s/FROM.*/FROM ghcr.io\/jgowdy\/bullseye/; t" -e "1,// s//FROM ghcr.io\/jgowdy\/bullseye/" debian/bullseye/curl/Dockerfile
-sed -i -e "1 s/FROM.*/FROM ghcr.io\/jgowdy\/bullseye-curl/; t" -e "1,// s//FROM ghcr.io\/jgowdy\/bullseye-curl/" debian/bullseye/scm/Dockerfile
-sed -i -e "1 s/FROM.*/FROM ghcr.io\/jgowdy\/bullseye-scm/; t" -e "1,// s//FROM ghcr.io\/jgowdy\/bullseye-scm/" debian/bullseye/Dockerfile
+sed -i -e "1 s/FROM.*/FROM ghcr.io\/golden-containers\/bullseye/; t" -e "1,// s//FROM ghcr.io\/golden-containers\/bullseye/" debian/bullseye/curl/Dockerfile
+sed -i -e "1 s/FROM.*/FROM ghcr.io\/golden-containers\/bullseye-curl/; t" -e "1,// s//FROM ghcr.io\/golden-containers\/bullseye-curl/" debian/bullseye/scm/Dockerfile
+sed -i -e "1 s/FROM.*/FROM ghcr.io\/golden-containers\/bullseye-scm/; t" -e "1,// s//FROM ghcr.io\/golden-containers\/bullseye-scm/" debian/bullseye/Dockerfile
 
 
