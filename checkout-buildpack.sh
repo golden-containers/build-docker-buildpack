39a9a53664edbbd6c12211cd56fefbe47a7ba1d5
Jeremiah Gowdy <jeremiah@gowdy.me>
2021-11-05 19:16:31 -0700

    Rewrite all bullseye files

--- checkout-buildpack.sh
+++ checkout-buildpack.sh
@@ -3,4 +3,7 @@ git clone --depth 1 --branch master https://github.com/docker-library/buildpack-
 cd buildpack-deps
 
 sed -i -e "1 s/FROM.*/FROM ghcr.io\/jgowdy\/bullseye/; t" -e "1,// s//FROM ghcr.io\/jgowdy\/bullseye/" debian/bullseye/curl/Dockerfile
+sed -i -e "1 s/FROM.*/FROM ghcr.io\/jgowdy\/bullseye-curl/; t" -e "1,// s//FROM ghcr.io\/jgowdy\/bullseye-curl/" debian/bullseye/scm/Dockerfile
+sed -i -e "1 s/FROM.*/FROM ghcr.io\/jgowdy\/bullseye-scm/; t" -e "1,// s//FROM ghcr.io\/jgowdy\/bullseye-scm/" debian/bullseye/Dockerfile
+
 
