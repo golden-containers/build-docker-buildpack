eedeea715dfb2a8c80676a77543eb255bf31d4f8
Jeremiah Gowdy <jeremiah@gowdy.me>
2021-11-11 11:11:48 -0800

    Fix path

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ cd buildpack-deps
 
 # Transform
 
-BASHBREW_SCRIPTS=.. ./apply-templates.sh
+BASHBREW_SCRIPTS=../.. ./apply-templates.sh
 
 sed -i -e "1 s/FROM.*/FROM ghcr.io\/golden-containers\/bullseye/; t" -e "1,// s//FROM ghcr.io\/golden-containers\/bullseye/" debian/bullseye/curl/Dockerfile
 sed -i -e "1 s/FROM.*/FROM ghcr.io\/golden-containers\/bullseye-curl/; t" -e "1,// s//FROM ghcr.io\/golden-containers\/bullseye-curl/" debian/bullseye/scm/Dockerfile
