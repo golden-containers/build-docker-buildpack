af89832872ffe31136d741b9eeeb907237e74144
Jeremiah Gowdy <jeremiah@gowdy.me>
2021-11-12 09:19:26 -0800

    Use label flag

--- build.sh
+++ build.sh
@@ -13,19 +13,16 @@ cd buildpack-deps
 # Transform
 
 sed -i -e "1 s/FROM.*/FROM ghcr.io\/golden-containers\/buildpack-deps:bullseye/; t" -e "1,// s//FROM ghcr.io\/golden-containers\/buildpack-deps:bullseye/" debian/bullseye/curl/Dockerfile
-echo "LABEL ${1:-DEBUG=TRUE}" >> debian/bullseye/curl/Dockerfile
 
 sed -i -e "1 s/FROM.*/FROM ghcr.io\/golden-containers\/buildpack-deps:bullseye-curl/; t" -e "1,// s//FROM ghcr.io\/golden-containers\/buildpack-deps:bullseye-curl/" debian/bullseye/scm/Dockerfile
-echo "LABEL ${1:-DEBUG=TRUE}" >> debian/bullseye/scm/Dockerfile
 
 sed -i -e "1 s/FROM.*/FROM ghcr.io\/golden-containers\/buildpack-deps:bullseye-scm/; t" -e "1,// s//FROM ghcr.io\/golden-containers\/buildpack-deps:bullseye-scm/" debian/bullseye/Dockerfile
-echo "LABEL ${1:-DEBUG=TRUE}" >> debian/bullseye/Dockerfile
 
 # Build
 
-docker build --tag ghcr.io/golden-containers/buildpack-deps:bullseye-curl debian/bullseye/curl
-docker build --tag ghcr.io/golden-containers/buildpack-deps:bullseye-scm debian/bullseye/scm
-docker build --tag ghcr.io/golden-containers/buildpack-deps:bullseye debian/bullseye
+docker build debian/bullseye/curl/ --tag ghcr.io/golden-containers/buildpack-deps:bullseye-curl --label ${1:-DEBUG=TRUE}
+docker build debian/bullseye/scm/ --tag ghcr.io/golden-containers/buildpack-deps:bullseye-scm --label ${1:-DEBUG=TRUE}
+docker build debian/bullseye/ --tag ghcr.io/golden-containers/buildpack-deps:bullseye --label ${1:-DEBUG=TRUE}
 
 # Push
 
